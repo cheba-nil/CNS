582c49867848ae9cc3526e3483fa9edf83db3b5d
Forrest <forrest.c.koch@gmail.com>
2019-05-06 18:30:23 +1000

    adjust documentation theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -187,3 +187,21 @@ epub_exclude_files = ['search.html']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-139625346-1',  #  Provided by Google in your dashboard
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
