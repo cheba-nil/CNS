4e9a2d08c17522b49980f8424b59b0546857c19f
Forrest Koch <forrest.koch@unsw.edu.au>
2019-06-11 15:52:25 +1000

    forgot semi colons

--- WMH_extraction/helper_funcs/subject_log.m
+++ WMH_extraction/helper_funcs/subject_log.m
@@ -1,4 +1,4 @@
 function subject_log (message,studyFolder,ID)
-    fp = fopen(strcat(studyFolder,'/subjects/',ID,'logfile.txt'))
-    fprintf(fp,message)
-    fclose(fp)
+    fp = fopen(strcat(studyFolder,'/subjects/',ID,'/logfile.txt'),'a');
+    fprintf(fp,message);
+    fclose(fp);
